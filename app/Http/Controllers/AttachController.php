d68d7ee89a3c05c2637d8ce4e2ac52b9ba906df1
Hazem Safwat <HSafwat@moj.gov.sa>
2023-01-01 01:27:13 +0300

    Finish the complains Module and the attachments files

--- app/Http/Controllers/AttachController.php
+++ app/Http/Controllers/AttachController.php
@@ -29,7 +29,7 @@ class AttachController extends Controller
     {
 
         $file = Attach::find($id);
-        $storagePath = storage_path('app\public\\' . $file['url']);
+        $storagePath = storage_path('app/public/' . $file['url']);
         $file->counter = $file->counter + 1;
         $file->save();
         //  return $storagePath;
@@ -41,7 +41,6 @@ class AttachController extends Controller
     {
 
         $file = Attach::find($id);
-        $storagePath = storage_path('app/public/' . $file['url']);
         $file->counter = $file->counter + 1;
         $file->save();
         $file['attachment_url'] = url('api/attach/' . $file['id']);
