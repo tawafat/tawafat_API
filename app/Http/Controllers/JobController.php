b3ec86d04fa076d0fda89c56dca4bdf6a843d28b
Hazem Safwat <HSafwat@moj.gov.sa>
2023-06-06 22:46:17 +0300

    try fix bug

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -552,15 +552,10 @@ class JobController extends Controller
         } else {
             // Create a new job detail
 
-            $validatedData['job_id'] =$jobId;
-            print_r($validatedData);
+            $validatedData['job_id'] = $jobId;
+            $validatedData['job_type'] = $job->type;
 
             $jobDetail = JobDetail::create($validatedData);
-            die();
-            $jobDetail->job_type = $job->type;
-            $jobDetail->job_id = "'".$jobId."'";
-
-            echo   $jobDetail->job_id;
 
             $jobDetail->save();
 
